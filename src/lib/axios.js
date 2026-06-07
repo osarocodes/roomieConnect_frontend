cdfe51c52a3f4d74838dd96096bcd0a6acaeb045
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2026-06-07 17:43:08 +0100

    chore: Minor fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api',
     withCredentials: true, // Ensure cookies are sent with requests
 });
-
-export default axiosInstance;
